4f2d0c8ebc6669884207f796d29cf02743264f0b
Rehmann Tyagi <irontyagi4@gmail.com>
2023-09-02 19:54:20 +0530

    delete extra route component in app compo

--- src/App.js
+++ src/App.js
@@ -4,28 +4,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // Site Pages
 import LoginPage from "./pages/LoginPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-// import Header from "./components/Header/Header.component";
-import SignUpForm from "./components/SignUpForm/SignUpForm.component";
 import NotFoundPage from "./pages/404.page";
+import SignUpForm from "./components/SignUpForm/SignUpForm.component";
 import Dashboard from './pages/Dashboard'
+// import Header from "./components/Header/Header.component";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/">
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/dashboard" element={<Dashboard />}>
-              <Route path="posts" element={<h1>Posts</h1>} />
-              <Route path="reels" element={<h1>Reels</h1>} />
-            </Route>
-            <Route path="forgot_password" element={<ForgotPasswordPage />} />
-            <Route path="sign_up" element={<SignUpForm />} />
-            <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="posts" element={<h1>Posts</h1>} />
+            <Route path="reels" element={<h1>Reels</h1>} />
           </Route>
+          <Route path="forgot_password" element={<ForgotPasswordPage />} />
+          <Route path="sign_up" element={<SignUpForm />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
-    </div >
+    </div>
   );
 }
 
